0c5365af2eaeca481d31484febd47752b917ebaf
Jorge Sánchez Blanco <JorgeSanchezBlanco1989@gmail.com>
2020-12-18 14:05:10 +0100

    Continued briefly

--- 19-Validation/process_form.php
+++ 19-Validation/process_form.php
@@ -1,1 +1,18 @@
 <?php
+include '../13-Includes/components/head.php';
+
+$error = true;
+if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['age']) && isset($_POST['email']) && isset($_POST['password'])){
+    $error = false;
+    extract($_POST);
+}
+
+?>
+<h1>$name</h1>
+    <h1>$surname</h1>
+    <h1>$age</h1>
+    <h1>$email</h1>
+    <h1>$password</h1>
+
+<?php
+include '../13-Includes/components/footer.php';
